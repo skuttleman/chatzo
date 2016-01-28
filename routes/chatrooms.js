6264c06fc106217f665d7566b724408e0d1a1b70
Ben Allred <skuttleman@gmail.com>
2016-01-28 15:08:31 -0700

    chatroom route

--- routes/chatrooms.js
+++ routes/chatrooms.js
@@ -1,2 +1,78 @@
 var route = require('express').Router();
+var knex = require('../db/knex');
 module.exports = route;
+
+
+// C
+route.post('/', function(request, response, next) {
+  if (request.user && request.user.id) {
+    request.body.user_id = request.user.id;
+    knex('chat_rooms').insert(request.body).then(function() {
+      response.json({ success: true });
+    }).catch(next);
+  } else {
+    next('You must be logged in to process this request');
+  }
+});
+
+// R
+route.get('/:id', function(request, response, next) {
+  if (request.user && request.user.id) {
+    knex('chat_rooms').where({ id: request.params.id, user_id: request.user.id })
+    .then(function(chat_rooms) {
+      if (chat_rooms.length) {
+        return knex('users')
+        .innerJoin('access', 'users.id', 'access.user_id')
+        .where(function() {
+          this.where('access.chat_room_id', request.params.id).orWhere('chat_rooms.is_private', false);
+        }).then(function(chat_rooms) {
+          response.json({ users: users, chat_rooms: chat_rooms });
+        });
+      } else {
+        next('You do not have access to this chat room');
+      }
+    }).catch(next);
+  } else {
+    next('You must be logged in to process this request');
+  }
+});
+
+// U
+route.put('/:id', function(request, response, next) {
+  knex('chat_rooms').where({ id: request.params.id }).then(function(chat_rooms) {
+    if (request.user && request.user.id && chat_rooms[0].user_id == request.user.id) {
+      request.body.user_id = request.user.id;
+      return knex('chat_rooms').update(request.body).where({ id: request.params.id })
+      .then(function(chat_rooms) {
+        response.json({ chat_rooms: chat_rooms });
+      });
+    } else {
+      next('You must be logged in to process this request');
+    }
+  }).catch(next);
+});
+
+// D
+route.delete('/:id', function(request, response, next) {
+  knex('chat_rooms').where({ id: request.params.id }).then(function(chat_rooms) {
+    if (request.user && request.user.id && chat_rooms[0].user_id == request.user.id) {
+      return knex('chat_rooms').where({ id: request.params.id }).del()
+      .then(function(chat_rooms) {
+        response.json({ success: true });
+      });
+    } else {
+      next('You must be logged in to process this request');
+    }
+  }).catch(next);
+});
+
+// L
+route.get('/', function(request, response, next) {
+  if (request.user && request.user.id) {
+    knex('chat_rooms').where({ user_id: request.user.id }).then(function(chat_rooms) {
+      response.json({ chat_rooms: chat_rooms });
+    }).catch(next);
+  } else {
+    next('You must be logged in to process this request');
+  }
+});
